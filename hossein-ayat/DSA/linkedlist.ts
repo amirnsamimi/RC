a632de76de249221651c4862d7b8c1231b3ddaca
amirnsamimi <amirnsamimi@gmail.com>
2025-07-20 21:51:41 +0330

    update: linkedlist completed

--- hossein-ayat/DSA/linkedlist.ts
+++ hossein-ayat/DSA/linkedlist.ts
@@ -1,31 +1,41 @@
 class ListNode {
-    value: number;
-    next: ListNode | null;
-  
-    constructor(value: number, next: ListNode | null = null) {
-      this.value = value
-      this.next = next
+  value: number;
+  next: ListNode | null;
+
+  constructor(value: number, next: ListNode | null = null) {
+    this.value = value;
+    this.next = next;
+  }
+}
+
+class LinkedList {
+  private head: ListNode | null;
+
+  append(value: number) {
+    const newNode = new ListNode(value);
+    if (!this.head) {
+      this.head = newNode;
     }
+    let current = this.head;
+    while (current.next !== null) {
+      current = current.next;
+    }
+    current.next = newNode;
   }
-  
-  class LinkedList {
-      private head: ListNode | null;
-      
-      append(value:number){
-          const newNode = new ListNode(value)
-          if(!this.head){
-              this.head = newNode
-          }
-          let current = this.head;
-          while( current.next !== null){
-              current = current.next
-          }
-          current.next = newNode
-          
-      }
-  
-  
-  
+
+  prepend(value: number) {
+    const newNode = new ListNode(value);
+    newNode.next = this.head;
+    this.head = newNode;
   }
-  
-  
+
+  print(): void {
+    let current = this.head;
+    const values:number[] = [];
+    while (current) {
+      values.push(current.value);
+      current = current.next;
+    }
+    console.log(values.join(" -> "));
+  }
+}
