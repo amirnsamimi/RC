d1cc298a434701a09f371557a2f8bccee97eba4f
amirnsamimi <amirnsamimi@gmail.com>
2025-07-24 12:44:14 +0330

    update: Code with sending back an string according to url

--- courses/mhadi-aliakbar/backend/http-server/src/main.ts
+++ courses/mhadi-aliakbar/backend/http-server/src/main.ts
@@ -1,6 +1,11 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
+  console.log(req.method, req.url);
+  if (req.url === "/hello") {
+    res.end("hello from server!");
+    return;
+  }
   res.end("hello world");
 });
 
