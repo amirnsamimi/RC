c92aa13e538c9c16fe06269f237563c913539c93
amirnsamimi <amirnsamimi@gmail.com>
2025-08-05 21:12:59 +0330

    update on auth, project plan

---
+++ workshops/hossein-ayat/backend/project/plan-project/learned.md
@@ -0,0 +1,12 @@
+## REQUESTS
+
+## POST 
+
+- typecheck, undefined check, validate
+- use zod for runtime check
+
+## POST LOGIN
+
+- check username, password
+- validate username, password
+- use string for ids 
--- workshops/hossein-ayat/backend/project/plan-project/src/main.ts
+++ workshops/hossein-ayat/backend/project/plan-project/src/main.ts
@@ -1,114 +1,120 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-import express, {Response, Request, NextFunction} from "express"
-import {z} from "zod";
+import express, { Response, Request, NextFunction } from "express";
+import { z } from "zod";
+import { v4 } from "uuid";
 
-
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Logging everytime a request comes in
 app.use((req: Request, res: Response, next: NextFunction) => {
-    console.log(req.method, req.url);
-    next();
-})
+  console.log(req.method, req.url);
+  next();
+});
 
 // User
 
-
-type TRoles = "Admin" | "Representative" | "Normal"
+type TRoles = "Admin" | "Representative" | "Normal";
 
 interface IUser {
-    id: string
-    username: string,
-    password: string
-    role: TRoles
+  id: string;
+  username: string;
+  password: string;
+  role: TRoles;
 }
 
 const userSchema = z.object({
-    username: z.string().min(1),
-    password: z.string().min(1),
-    role: z.enum(["Admin", "Representative", "Normal"])
-})
+  username: z.string().min(1),
+  password: z.string().min(1),
+  role: z.enum(["Admin", "Representative", "Normal"]),
+});
 
-const users: IUser[] = [];
+const users: IUser[] = [
+  { id: v4(), username: "admin", password: "admin", role: "Admin" },
+];
 
 app.post("/login", (req: Request, res: Response) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).json({ message: "bad request" });
+    return;
+  }
 
-    const {username, password} = req.body
-    if (!username || !password) {
-        res.status(400).json({message: "bad request"});
-        return;
-    }
+  const parseResult = userSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    res.status(400).json({ message: parseResult.error.message });
+    return;
+  }
 
-    const parseResult = userSchema.safeParse(req.body)
-    if (!parseResult.success) {
-        res.status(400).json({message: parseResult.error.message})
-        return;
-    }
+  const userFound = users.find(
+    (user) => user.username === username && user.password === password
+  );
+  if (!userFound) {
+    res.status(401).json({ message: "Invalid username or password" });
+    return;
+  }
 
-    const userFound = users.find(user => user.username === username && user.password === password)
-    if (!userFound) {
-        res.status(401).json({message: "Invalid username or password"})
-        return;
-    }
-
-    res.status(200).send({message: "Login successful", user: userFound})
-
-})
-
+  res.status(200).send({ message: "Login successful", user: userFound });
+});
 
 // Plans
 interface IPlan {
-    id: number;
-    title: string;
-    description?: string;
+  id: number;
+  title: string;
+  description?: string;
 }
 
 const planSchema = z.object({
-    title: z.string().min(1),
-    description: z.string().min(1).optional()
-})
+  title: z.string().min(1),
+  description: z.string().min(1).optional(),
+});
 const plans: IPlan[] = [];
 app.post("/plan", (req: Request, res: Response) => {
-    const {title, description} = req.body
-    if (req.body.title === undefined) {
-        res.status(400).json({message: "Please enter a title"});
-        return;
-    }
-    const parseResult = planSchema.safeParse(req.body);
-    if (!parseResult.success) {
-        res.status(400).json({message: parseResult.error.message});
-        return;
-    }
-    const plan: IPlan = {id: plans.length + 1, title, description}
-    plans.push(plan)
-    res.status(200).send({message: `Plan with id ${plan.id} added successfully.`})
-})
+  const userId = req.headers["authorization"];
+  const { title, description } = req.body;
+  const logged = users.find((user) => user.id === userId);
+  if (!logged) {
+    res.status(401).send({ message: "unauthorized" });
+  }
+
+  if (req.body.title === undefined) {
+    res.status(400).json({ message: "Please enter a title" });
+    return;
+  }
+  const parseResult = planSchema.safeParse(req.body);
+  if (!parseResult.success) {
+    res.status(400).json({ message: parseResult.error.message });
+    return;
+  }
+  const plan: IPlan = { id: plans.length + 1, title, description };
+  plans.push(plan);
+  res
+    .status(200)
+    .send({ message: `Plan with id ${plan.id} added successfully.` });
+});
 
 app.get("/plan/:id", (req: Request, res: Response) => {
-    const id = Number(req.params.id)
-    if (Number.isNaN(id)) {
-        res.status(400).json({message: "Please enter a valid id"});
-        return;
-    }
-    const plan = plans.find(plan => plan.id === id)
-    if (!plan) {
-        res.status(404).json({message: "Plan not found"})
-        return;
-    }
-    res.status(200).send(plan)
-})
-
+  const id = Number(req.params.id);
+  if (Number.isNaN(id)) {
+    res.status(400).json({ message: "Please enter a valid id" });
+    return;
+  }
+  const plan = plans.find((plan) => plan.id === id);
+  if (!plan) {
+    res.status(404).json({ message: "Plan not found" });
+    return;
+  }
+  res.status(200).send(plan);
+});
 
 // Not Found Middleware
 app.use((req: Request, res: Response) => {
-    res.status(404).send("Not Found");
-})
+  res.status(404).send("Not Found");
+});
 // Server Port Opening
 app.listen(process.env.LISTENING_PORT, () => {
-    console.log("server running on port", process.env.LISTENING_PORT);
-
-})
+  console.log("server running on port", process.env.LISTENING_PORT);
+});
