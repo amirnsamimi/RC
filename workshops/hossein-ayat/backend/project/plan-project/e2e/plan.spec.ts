35b91d80eedc8f7767afea6e60fe0380e3c2fa0e
amirnsamimi <amirnsamimi@gmail.com>
2025-08-06 22:42:22 +0330

    test updated for getting plans by id

--- workshops/hossein-ayat/backend/project/plan-project/e2e/plan.spec.ts
+++ workshops/hossein-ayat/backend/project/plan-project/e2e/plan.spec.ts
@@ -1,26 +1,54 @@
 import request from "supertest";
-import { app } from "../src/api";
+import {app} from "../src/api";
 
-describe("craeting plan", () => {
-  test("failed if user is not logged in", async () => {
-    // promise in return
-    await request(app).post("/plan").expect(401);
-  });
+describe("creating plan", () => {
+    test("failed if user is not logged in", async () => {
+        // promise in return
+        await request(app).post("/plan").expect(401);
+    });
 
-  test("should create a plan if we are logged in", async () => {
-    const { body: user } = await request(app)
-      .post("/login")
-      .send({ username: "admin", password: "admin" })
-      .expect(200);
-    const { body: plan } = await request(app)
-      .post("/plan")
-      .set("Authorization", user.id)
-      .send({
-        title: "plan 2",
-        description: "plan 2 is awsome",
-      })
-      .expect(200);
-      expect(plan.title).toBe("plan 2")
- 
-  });
+    test("should create a plan if we are logged in", async () => {
+        const {body: user} = await request(app)
+            .post("/login")
+            .send({username: "admin", password: "admin"})
+            .expect(200);
+        const {body: plan} = await request(app)
+            .post("/plan")
+            .set("Authorization", user.id)
+            .send({
+                title: "plan 2",
+                description: "plan 2 is awsome",
+            })
+            .expect(200);
+        expect(plan.title).toBe("plan 2")
+
+    });
 });
+
+describe("getting plan", () => {
+    test("get plan by id", async () => {
+
+        const {body: user} = await request(app)
+            .post("/login")
+            .send({username: "admin", password: "admin"})
+            .expect(200);
+
+
+        const {body: plan} = await request(app)
+            .post("/plan")
+            .set("Authorization", user.id)
+            .send({
+                title: "plan 2",
+                description: "plan 2 is awesome",
+            })
+            .expect(200);
+        const {body: plan2} = await request(app)
+            .get(`/plan/${plan.id}`)
+            .expect(200);
+
+        console.log(plan2)
+
+    })
+
+
+})
